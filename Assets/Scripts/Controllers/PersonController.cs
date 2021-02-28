c2d34090de08004fd0d7f9525eb9aa8be44f1ebc
Tim <ryalin1286@gmail.com>
2021-02-28 12:16:02 +0000

    got player grid movement working

--- Assets/Scripts/Controllers/PersonController.cs
+++ Assets/Scripts/Controllers/PersonController.cs
@@ -11,6 +11,10 @@ public class PersonController : MonoBehaviour
     void Start()
     {
         movePoint.parent = null;
+
+        Grid grid = transform.parent.GetComponent<Grid>();
+        Vector3Int cellPosition = grid.WorldToCell(transform.position);
+        transform.position = grid.GetCellCenterWorld(cellPosition);
     }
 
     // Update is called once per frame
@@ -18,13 +22,17 @@ public class PersonController : MonoBehaviour
     {
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
-        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
         {
-            movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+            }
+            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+            }
         }
-        if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
-        {
-            movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
-        }
+
     }
 }
